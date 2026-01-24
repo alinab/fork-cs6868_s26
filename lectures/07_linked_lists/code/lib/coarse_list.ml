98d1571143eff192ef8167e73ccb6ec93a62be4a
KC Sivaramakrishnan <kc@kcsrk.info>
2026-01-24 22:53:46 +0530

    updates

--- lectures/07_linked_lists/code/lib/coarse_list.ml
+++ lectures/07_linked_lists/code/lib/coarse_list.ml
@@ -7,9 +7,9 @@
 
 (** Internal node representation *)
 type 'a node = {
-  item : 'a option;              (* None for sentinel nodes *)
-  key : int;                     (* hash code for the item, or min_int/max_int for sentinels *)
-  mutable next : 'a node option; (* next node in the list, None = null *)
+  item : 'a option;         (* None for sentinel nodes *)
+  key : int;                (* hash code for the item, or min_int/max_int for sentinels *)
+  mutable next : 'a node;   (* next node in the list, tail points to itself *)
 }
 
 (** The coarse-grained list type *)
@@ -21,43 +21,40 @@ type 'a t = {
 
 (** Create a new empty list with sentinel nodes *)
 let create () =
-  let rec head = { item = None; key = min_int; next = Some tail }
-  and tail = { item = None; key = max_int; next = None } in
+  let rec head = { item = None; key = min_int; next = tail }
+  and tail = { item = None; key = max_int; next = tail } in
   { head; tail; lock = Mutex.create () }
+
+(** Helper to execute a function while holding the lock *)
+let with_lock list f =
+  Mutex.lock list.lock;
+  Fun.protect ~finally:(fun () -> Mutex.unlock list.lock) f
 
 (** Add an element to the list *)
 let add list item =
   let key = Hashtbl.hash item in
-  Mutex.lock list.lock;
-  Fun.protect ~finally:(fun () -> Mutex.unlock list.lock) @@ fun () ->
+  with_lock list @@ fun () ->
     let rec traverse pred curr =
       if curr.key < key then
-        match curr.next with
-        | None -> assert false  (* Should not happen with sentinels *)
-        | Some next -> traverse curr next
+        traverse curr curr.next
       else if curr.key = key then
         false  (* element already present *)
       else begin
         (* insert new node between pred and curr *)
-        let node = { item = Some item; key; next = Some curr } in
-        pred.next <- Some node;
+        let node = { item = Some item; key; next = curr } in
+        pred.next <- node;
         true
       end
     in
-    match list.head.next with
-    | None -> assert false  (* Should not happen *)
-    | Some first -> traverse list.head first
+    traverse list.head list.head.next
 
 (** Remove an element from the list *)
 let remove list item =
   let key = Hashtbl.hash item in
-  Mutex.lock list.lock;
-  Fun.protect ~finally:(fun () -> Mutex.unlock list.lock) @@ fun () ->
+  with_lock list @@ fun () ->
     let rec traverse pred curr =
       if curr.key < key then
-        match curr.next with
-        | None -> assert false  (* Should not happen with sentinels *)
-        | Some next -> traverse curr next
+        traverse curr curr.next
       else if curr.key = key then begin
         (* element found, remove it *)
         pred.next <- curr.next;
@@ -65,23 +62,16 @@ let remove list item =
       end else
         false  (* element not present *)
     in
-    match list.head.next with
-    | None -> assert false  (* Should not happen *)
-    | Some first -> traverse list.head first
+    traverse list.head list.head.next
 
 (** Test whether an element is present *)
 let contains list item =
   let key = Hashtbl.hash item in
-  Mutex.lock list.lock;
-  Fun.protect ~finally:(fun () -> Mutex.unlock list.lock) @@ fun () ->
+  with_lock list @@ fun () ->
     let rec traverse curr =
       if curr.key < key then
-        match curr.next with
-        | None -> assert false  (* Should not happen with sentinels *)
-        | Some next -> traverse next
+        traverse curr.next
       else
         curr.key = key
     in
-    match list.head.next with
-    | None -> assert false  (* Should not happen *)
-    | Some first -> traverse first
+    traverse list.head.next
