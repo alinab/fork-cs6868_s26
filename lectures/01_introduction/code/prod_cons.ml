d23375fad6a217c05e4a66e22b8ccb3640b4c387
Alina Banerjee <alina@glitchgirl.us>
2026-01-27 13:56:00 +0530

    Replace while loops with an unless function; mutexes have been removed

--- lectures/01_introduction/code/prod_cons.ml
+++ lectures/01_introduction/code/prod_cons.ml
@@ -1,4 +1,5 @@
 (* Producer-Consumer with Alice and Bob using Can Protocol *)
+let pr = Printf.printf
 
 (* Different types of fish *)
 type fish = Salmon | Trout | Bass | Catfish | Tuna
@@ -23,6 +24,9 @@ type can_state = Up | Down
 
 type can = { mutable state : can_state }
 
+(* Initial state for the can is Up. In our single producer-
+   single consumer scenario, the can has to knocked down
+   after food into the pond *)
 let create_can () = { state = Up }
 
 let is_up can = can.state = Up
@@ -36,46 +40,51 @@ let knock_over can = can.state <- Down
 (* Shared pond with current fish *)
 type pond = { mutable food : fish option }
 
+(* At the start, the pond has no fish; this setup is correct
+   with the can being initally up *)
 let create_pond () = { food = None }
 
 let stock_pond pond fish = pond.food <- Some fish
 
 let get_food pond = pond.food
 
+let unless cond item f =
+    let rec loop () =
+        if (cond item)
+        then let _ = f () in
+             loop ()
+        else loop ()
+    in
+    loop ()
+
 (* Alice (Consumer) - releases pets to eat fish *)
 let alice can pond =
-  while true do
-    while is_up can do
-      ()
-    done;
-    (match get_food pond with
-    | Some fish ->
-        Printf.printf "Alice: Releasing pets to eat %s\n%!" (fish_to_string fish);
-        Unix.sleepf 0.1;
-        Printf.printf "Alice: Recapturing pets\n%!"
-    | None -> failwith "impossible");
-    reset can
-  done
+    unless is_down can (fun _ ->
+      (match get_food pond with
+      | Some fish ->
+          pr "Alice: Releasing pets to eat %s\n%!" (fish_to_string fish);
+          Unix.sleepf 0.1;
+          pr "Alice: Recapturing pets\n%!"
+      | None -> failwith "impossible");
+      reset can
+    )
 
 (* Bob (Producer) - stocks the pond with fish *)
 let bob can pond =
-  while true do
-    while is_down can do
-      ()
-    done;
-    let fish = random_fish () in
-    Printf.printf "Bob: Stocking pond with %s\n%!" (fish_to_string fish);
-    stock_pond pond fish;
-    knock_over can
-  done
+    unless is_up can (fun _ ->
+      let fish = random_fish () in
+      pr "Bob: Stocking pond with %s\n%!" (fish_to_string fish);
+      stock_pond pond fish;
+      knock_over can
+    )
 
 (* Main program *)
 let () =
   Random.self_init ();
   let can = create_can () in
   let pond = create_pond () in
-  Printf.printf "Starting Producer-Consumer with Can Protocol...\n%!";
-  Printf.printf "Bob produces fish, Alice's pets consume them.\n\n%!";
+  pr "Starting Producer-Consumer with Can Protocol...\n%!";
+  pr "Bob produces fish, Alice's pets consume them.\n\n%!";
   (* Spawn Alice's domain (consumer) *)
   let alice_domain = Domain.spawn (fun () -> alice can pond) in
   (* Spawn Bob's domain (producer) *)
