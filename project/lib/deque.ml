b7bdeac9167f5fbeaee63e81deceaf0c43733d98
Alina Banerjee <alina@glitchgirl.us>
2026-03-30 17:17:47 +0530

    Add types and functions to define a deque
    The following types and functions have been added:
      - result
      - the deque type itself
      - create (deque)
      - cas_top for CAS operation on the index stored in top
      - push_bottom to push an element to the bottom
      - steal an element from the top end
      - pop_bottom to pop an element from the bottom end

---
+++ project/lib/deque.ml
@@ -0,0 +1,84 @@
+type 'a result =
+    | Empty
+    | Abort
+    | Value of 'a
+
+type 'a t = {
+    bottom : int Atomic.t;
+    top    : int Atomic.t;
+    active_array : 'a Circular_array.t Atomic.t;
+}
+
+let create log_initial_size = {
+    bottom  = Atomic.make 0;
+    top     = Atomic.make 0;
+    active_array = Atomic.make (Circular_array.create log_initial_size);
+}
+
+let cas_top deque_arr old_top new_top =
+    Atomic.compare_and_set deque_arr.top old_top new_top
+
+let push_bottom deq task =
+   let b = Atomic.get deq.bottom in
+   let t = Atomic.get deq.top in
+   let a = ref (Atomic.get deq.active_array) in
+   let size = b - t in
+   if size >= Circular_array.size !a - 1 then begin
+       a := Circular_array.grow !a ~bottom:b ~top:t;
+       Atomic.set deq.active_array !a;
+   end;
+   Circular_array.put_item !a b task;
+   Atomic.set deq.bottom (b + 1)
+
+
+let steal deq =
+    let t = Atomic.get deq.top in
+    let b = Atomic.get deq.bottom in
+    let a = Atomic.get deq.active_array in
+    let size = b - t in
+    if size <= 0 then Empty
+    else
+      let stolen_task = Circular_array.get_item a t in
+      if not (cas_top deq t (t + 1))
+      then Abort
+      else Value stolen_task
+
+(* Constant used for shrinking the array *)
+let k = 3
+
+let perhaps_shrink deq ~bottom ~top =
+    let a = Atomic.get deq.active_array in
+    let num_elements = bottom - top in
+    let curr_size = Circular_array.size a in
+    if (curr_size > 1 && num_elements < (curr_size / k)) then
+       Circular_array.shrink a ~bottom:bottom ~top:top
+    else
+        a
+
+let pop_bottom deq =
+    let b = Atomic.get deq.bottom - 1 in
+    let a = Atomic.get deq.active_array in
+    Atomic.set deq.bottom b;
+    let t = Atomic.get deq.top in
+    let size = b - t in
+    if (size < 0) then begin
+        Atomic.set deq.bottom t; (* empty deque, top = bottom *)
+        Empty                    (* nothing to return *)
+    end
+    else
+      let task = Circular_array.get_item a b in
+      if (size > 0) then begin
+          let a' = perhaps_shrink deq ~bottom:b ~top:t
+          in Atomic.set deq.active_array a';
+          Value task
+      end
+      (* size = 1 i.e. popping the last remaining element races with thieves
+         trying to steal the last element *)
+      else begin
+          let result = if cas_top deq t (t + 1)
+                       then Value task
+                       else Empty (* some thief stole the last element *)
+          in
+          Atomic.set deq.bottom (t + 1);
+          result
+      end
